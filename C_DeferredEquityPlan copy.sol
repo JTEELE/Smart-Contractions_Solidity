c80198e9883e2cbdd796fe59ad459abdf7726909
JTEELE <jteele21@gmail.com>
2021-12-19 23:32:37 -0600

    initial commit of solidity script and README, requirements.txt upload to follow

---
+++ C_DeferredEquityPlan copy.sol
@@ -0,0 +1,53 @@
+pragma solidity ^0.5.0;
+
+// lvl 3: equity plan
+contract DeferredEquityPlan {
+    uint fakenow = now;
+    
+    function fastforward() public {
+        fakenow += 100 days;
+    }
+
+    address human_resources;
+
+    address payable employee; // bob
+    bool active = true; // this employee is active at the start of the contract
+
+    // @TODO: Set the total shares and annual distribution
+    uint total_shares = 1000;
+    uint annual_distribution = 250; // Four-year vesting period (250 shares/yr. = 1000 total_shares)
+
+    uint start_time = now; // permanently store the time this contract was initialized
+
+    uint unlock_time = now + 365 days;
+
+    uint public distributed_shares; // starts at 0
+
+    constructor(address payable _employee) public {
+        human_resources = msg.sender;
+        employee = _employee;
+    }
+
+    function distribute() public {
+        require(msg.sender == human_resources || msg.sender == employee, "You are not authorized to execute this contract.");
+        require(active == true, "Contract not active.");
+        require(unlock_time < now, "Contract must be locked for 365 Days.");
+        require(distributed_shares < total_shares, "Employee has reached maximum allowance in Stock comp plan");
+        unlock_time = unlock_time + 365 days;
+        distributed_shares = (now - start_time) / 365 days * annual_distribution;
+        if (distributed_shares > 1000) {
+            distributed_shares = 1000;
+        }
+    }
+
+    // human_resources and the employee can deactivate this contract at-will
+    function deactivate() public {
+        require(msg.sender == human_resources || msg.sender == employee, "You are not authorized to deactivate this contract.");
+        active = false;
+    }
+
+    // Revert any Ether back to user (not handled in this script)
+    function() external payable {
+        revert("Do not send Ether to this contract!");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# Smart-Contractions_Solidity
-Automate corporate accounting processes with Solidity Smart Contracts
+># *Automate Corporate Processes with Smart Contracts*  
+
+<img src="Images/black_hole.jpg" align="center" width="200px"/>
+<br clear="center"/>
+<img src="Images/ETHUSD2.png" align="right" width="350px"/>
+<img src="Images/solidity.png" align="right" width="350px"/>
+
+>### Overview: 
+>> These scripts will *Compile* & *Execute* solidity smart contracts to execute traditional _Accounts Payable_ functions.
+
+#### Equal allocation: _AssociateProfitSplitter_
+
+#### Hierarchical allocation: _TieredProfitSplitter_
+
+#### Stock Compensation: _DeferredEquityPlan_
+
+>## What is Solidity?
+> [Solidity](https://docs.soliditylang.org/en/v0.8.10/#): "Solidity is an object-oriented, high-level language for implementing smart contracts. Smart contracts are programs which govern the behaviour of accounts within the Ethereum state."
+
+>## What are Smart Contracts?
+> [Data Science](https://medium.com/coinmonks/what-are-smart-contracts-definition-and-explanation-e96cb879a8cb): "Smart contracts are electronic contracts based on a computer program that automate business processes in a legally secure way, also between partner companies. Smart contracts are based on blockchain technology and use this technology's advantages, such as transparency, forgery protection, and availability."
+>
+
+## Resources:
+1. Northwestern NPS:
+https://sps.northwestern.edu/
+
+2. Solidity:
+https://docs.soliditylang.org/en/v0.8.10/#
+
+3. Medium:
+https://medium.com/coinmonks/what-are-smart-contracts-definition-and-explanation-e96cb879a8cb
